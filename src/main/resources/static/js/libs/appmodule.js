5b86af980102969a029501a176cdb47c5cd13171
pipegomez <juan.gomez-r@hotmail.com>
2015-11-12 13:03:50 -0500

    LaboratorioenClase

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -1,5 +1,70 @@
 (function () {
     var app = angular.module('modone', []);
+	app.controller('plan_control', 
+    function($scope,$log, $http){
+		$scope.seleccionado = "";
+		$scope.listaPlanos = [];
+
+    
+	
+		$scope.loadData = function() {
+			var configList = {
+				method: "GET",
+				url: "blueprints"
+			};
+
+			var response=$http(configList);
+
+			response.success(function(data, status, headers, config) {
+				 $log.debug('se cargo');
+				$scope.listaPlanos = data;
+			});
+
+			response.error(function(data, status, headers, config) {
+				alert("The petition has failed. HTTP Status:"+status);
+			});
+		};
+
+		$scope.loadDataSelec = function() {
+			var configList = {
+				method: "GET",
+				url: "blueprints/"+$scope.seleccionado
+			};
+
+			var response=$http(configList);
+
+			response.success(function(data, status, headers, config) {
+				$log.debug('se cargo');
+				var cnv= document.getElementById("myCanvas");
+				var ctx = cnv.getContext("2d");
+								
+				for(i=0; i < data.points.length-1; i++){
+					alert(i);
+					ctx.moveTo(data.points[i].x,data.points[i].y);
+					ctx.lineTo(data.points[i+1].x,data.points[i+1].y);
+				}
+				
+				ctx.strokeStyle = "#f00";
+				ctx.stroke();
+				
+				$scope.listaPlanos = data;
+				
+			});
+
+			response.error(function(data, status, headers, config) {
+				alert("The petition has failed. HTTP Status:"+status);
+			});
+		};
+
+/*
+{points: "points",								name: "name"}
+*/
+
+    }
+    );
+
+	
+
 })();
 
 
